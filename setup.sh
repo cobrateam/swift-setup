99bfaa55446b24b3d9ad94d54045d60513eb0794
Andrews Medina <andrewsmedina@gmail.com>
2012-01-18 09:45:02 -0200

    installing deps

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#sudoer
+sudo su
+
+#installing deps
+apt-get install python-software-properties
+add-apt-repository ppa:swift-core/release
+apt-get update
+apt-get install curl gcc git-core memcached python-configobj python-coverage python-dev python-nose python-setuptools python-simplejson python-xattr sqlite3 xfsprogs python-webob python-eventlet python-greenlet python-pastedeploy python-netifaces
+
+
